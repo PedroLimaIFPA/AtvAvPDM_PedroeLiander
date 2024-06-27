0d3d962b3a569f04060bb160e87852843803b064
Liander Souza <liandervinicius13@gmail.com>
2024-06-27 15:00:02 -0300

    telas configuradas e rotas funcionando

--- js/app.js
+++ js/app.js
@@ -3,4 +3,19 @@ const circle = document.querySelector('.circle')
 
 theme.addEventListener('click', () => {
     circle.classList.toggle('theme-active')
+
+    const savedMode = localStorage.getItem('mode');
+    if (savedMode) {
+      document.body.classList.add(savedMode);
+    }
+  
+    theme.addEventListener('click', () => {
+      document.body.classList.toggle('light');
+      let mode = 'light-mode';
+      if (document.body.classList.contains('light')) {
+        mode = 'light';
+      }
+      // Save the current mode to local storage
+      localStorage.setItem('mode', mode);
+    });
 })
